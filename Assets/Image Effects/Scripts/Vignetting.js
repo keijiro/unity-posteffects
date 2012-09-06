691683068f29cb5959a53c0f161f1e9235b668b9
Keijiro Takahashi <keijiro@gmail.com>
2012-09-06 10:38:11 +0900

    Simplify the vignetting fx.

--- Assets/Image Effects/Scripts/Vignetting.js
+++ Assets/Image Effects/Scripts/Vignetting.js
@@ -11,19 +11,15 @@ class Vignetting extends PostEffectsBase {
 
     @HideInInspector var vignetteShader : Shader;
     private var vignetteMaterial : Material;
-    @HideInInspector var separableBlurShader : Shader;
-    private var separableBlurMaterial : Material;
 
     function CheckResources() {
-        CheckSupport();
         vignetteMaterial = CheckShaderAndCreateMaterial(vignetteShader, vignetteMaterial);
-        separableBlurMaterial = CheckShaderAndCreateMaterial(separableBlurShader, separableBlurMaterial);
-        if (!isSupported) ReportAutoDisable();
-        return isSupported;
+        return CheckSupport();
     }
 
     function OnRenderImage(source : RenderTexture, destination : RenderTexture) {
         if (!CheckResources()) {
+            ReportAutoDisable();
             Graphics.Blit(source, destination);
             return;
         }
@@ -31,25 +27,25 @@ class Vignetting extends PostEffectsBase {
         var widthOverHeight = (1.0 * source.width) / source.height;
         var oneOverBaseSize = 1.0 / 512;
 
-        var halfRezColor = RenderTexture.GetTemporary(source.width / 2, source.height / 2, 0);
-        var quarterRezColor = RenderTexture.GetTemporary(source.width / 4, source.height / 4, 0);
-        var secondQuarterRezColor = RenderTexture.GetTemporary(source.width / 4, source.height / 4, 0);
+        var halfRez = RenderTexture.GetTemporary(source.width / 2, source.height / 2, 0);
+        var quarterRez = RenderTexture.GetTemporary(source.width / 4, source.height / 4, 0);
+        var vignetted = RenderTexture.GetTemporary(source.width / 4, source.height / 4, 0);
 
-        Graphics.Blit(source, halfRezColor);
-        Graphics.Blit(halfRezColor, quarterRezColor);
+        Graphics.Blit(source, halfRez);
+        Graphics.Blit(halfRez, quarterRez);
 
-        separableBlurMaterial.SetVector("offsets", Vector4(0.0, blurSpread * oneOverBaseSize, 0.0, 0.0));
-        Graphics.Blit(quarterRezColor, secondQuarterRezColor, separableBlurMaterial, 0);
-        separableBlurMaterial.SetVector("offsets", Vector4(blurSpread * oneOverBaseSize / widthOverHeight, 0.0, 0.0, 0.0));
-        Graphics.Blit(secondQuarterRezColor, quarterRezColor, separableBlurMaterial, 0);
+        vignetteMaterial.SetVector("offsets", Vector4(0, blurSpread * oneOverBaseSize, 0, 0));
+        Graphics.Blit(quarterRez, vignetted, vignetteMaterial, 1);
+        vignetteMaterial.SetVector("offsets", Vector4(blurSpread * oneOverBaseSize / widthOverHeight, 0, 0, 0));
+        Graphics.Blit(vignetted, quarterRez, vignetteMaterial, 1);
 
         vignetteMaterial.SetFloat("intensity", intensity);
         vignetteMaterial.SetFloat("blur", blur);
-        vignetteMaterial.SetTexture("vignetteTex", quarterRezColor);
+        vignetteMaterial.SetTexture("vignetteTex", quarterRez);
         Graphics.Blit(source, destination, vignetteMaterial, 0);
         
-        RenderTexture.ReleaseTemporary(halfRezColor);           
-        RenderTexture.ReleaseTemporary(quarterRezColor);    
-        RenderTexture.ReleaseTemporary(secondQuarterRezColor);  
+        RenderTexture.ReleaseTemporary(halfRez);           
+        RenderTexture.ReleaseTemporary(quarterRez);    
+        RenderTexture.ReleaseTemporary(vignetted);  
     }
 }
--- Assets/Image Effects/Shaders/VignettingShader.shader
+++ Assets/Image Effects/Shaders/VignettingShader.shader
@@ -4,6 +4,8 @@ Shader "Post FX/VignettingShader" {
 	}
 
 	Subshader {
+
+		// Vignetting pass
 		Pass {
 			Cull Off
 			ZTest Always
@@ -36,6 +38,46 @@ Shader "Post FX/VignettingShader" {
 
 				lowp float mask = 1.0 - coord2 * intensity * 0.1;
 				gl_FragColor = mix(color, colorBlur, min(blur * coord2, 1.0)) * mask;
+			}
+			#endif
+
+			ENDGLSL
+		}
+
+		// Blur pass
+		Pass {
+			Cull Off
+			ZTest Always
+			ZWrite Off
+			Fog { Mode off }
+
+			GLSLPROGRAM
+
+			uniform sampler2D _MainTex;
+			uniform vec4 offsets;
+			varying vec2 uv;
+			varying vec4 delta[3];
+
+			#ifdef VERTEX
+			void main() {
+	            gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+				uv = gl_MultiTexCoord0.xy;
+				delta[0] = gl_MultiTexCoord0.xyxy + offsets.xyxy * vec4(1, 1, -1, -1);
+				delta[1] = gl_MultiTexCoord0.xyxy + offsets.xyxy * vec4(2, 2, -2, -2);
+				delta[2] = gl_MultiTexCoord0.xyxy + offsets.xyxy * vec4(3, 3, -3, -3);
+			}
+			#endif
+
+			#ifdef FRAGMENT
+			void main() {
+				gl_FragColor =
+					0.4  * texture2D(_MainTex, uv) +
+					0.15 * texture2D(_MainTex, delta[0].xy) +
+					0.15 * texture2D(_MainTex, delta[0].zw) +
+					0.1  * texture2D(_MainTex, delta[1].xy) +
+					0.1  * texture2D(_MainTex, delta[1].zw) +
+					0.05 * texture2D(_MainTex, delta[2].xy) +
+					0.05 * texture2D(_MainTex, delta[2].zw);
 			}
 			#endif
 
